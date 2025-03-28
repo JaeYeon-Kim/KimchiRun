bdbd7fef703d4d233bd5a6cf432d90aed24e235d
JaeYoun Kim <rlawodusx@naver.com>
2025-03-29 01:04:08 +0900

    Feat: 배경 스크롤 기능 추가

--- Assets/Scripts/BackgroundScroll.cs
+++ Assets/Scripts/BackgroundScroll.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
 
+
+// 배경 스크롤 코드 
 public class BackgroundScroll : MonoBehaviour
 {
     [Header("Settings")]
+    [Tooltip("How fast should the texture scroll")]
     public float scrollSpeed;
+
+    [Header("References")]
+    public MeshRenderer meshRenderer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -11,9 +17,12 @@ public class BackgroundScroll : MonoBehaviour
         
     }
 
+    
+
     // Update is called once per frame
     void Update()
     {
-        
+        // offset을 변경하여 움직이는 효과 재생 
+        meshRenderer.material.mainTextureOffset += new Vector2(scrollSpeed * Time.deltaTime, 0);
     }
 }
