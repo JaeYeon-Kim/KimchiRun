12f5764f6e13a36b1ab8f79e423a0e1e042f64ae
JaeYoun Kim <rlawodusx@naver.com>
2025-04-03 19:13:33 +0900

    Feat: 게임매니저 전까지 구현

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,15 +8,21 @@ public class Player : MonoBehaviour
 
     [Header("References")]
     public Rigidbody2D playerRigidBody;
-    public Animator playerAnimator;
+    public Animator PlayerAnimator;
+
+    public BoxCollider2D PlayerCollider;
+
 
     // 땅에 닿았는지 판별하는 변수
     private bool isGrounded = true;
 
+    private int lives = 3;
+    private bool isInvincible = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -24,22 +31,85 @@ public class Player : MonoBehaviour
         // 키 입력 감지 
 
         // 스페이스바를 눌렀을 경우 
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
             playerRigidBody.AddForceY(jumpForce, ForceMode2D.Impulse);
             isGrounded = false;
-            playerAnimator.SetInteger("state", 1);
-        
+            PlayerAnimator.SetInteger("state", 1);
+
         }
+    }
+
+    void KillPlayer()
+    {
+        PlayerCollider.enabled = false;
+        PlayerAnimator.enabled = false;
+        playerRigidBody.AddForceY(jumpForce, ForceMode2D.Impulse);
+    }
+
+
+    void Hit()
+    {
+        lives -= 1;
+        // 체력이 0이하가 되면 플레이어 사망 
+        if (lives <= 0)
+        {
+            KillPlayer();
+        }
+    }
+
+    void Heal()
+    {
+        // 이미 3개 이상일경우에는 늘어나면 안됨 
+        lives = Mathf.Min(3, lives + 1);       // 3으로 값을 제한, 3이상의 값이 들어와도 3을 반환
+    }
+
+    void StartInvincible()
+    {
+        isInvincible = true;
+        Invoke("StopInvincible", 5f);
+    }
+
+    void StopInvincible()
+    {
+        isInvincible = false;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.name == "Platform") {
-            if(!isGrounded){
-              playerAnimator.SetInteger("state", 2);
+        if (collision.gameObject.name == "Platform")
+        {
+            if (!isGrounded)
+            {
+                PlayerAnimator.SetInteger("state", 2);
             }
             isGrounded = true;
-   
+
         }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // enemy와 부딪혔을때 
+        if (collision.gameObject.tag == "enemy")
+        {
+            if (!isInvincible)
+            {
+                Destroy(collision.gameObject);
+                Hit();
+            }
+
+        }
+        else if (collision.gameObject.tag == "food")
+        {
+            Destroy(collision.gameObject);
+            Heal();
+        }
+        else if (collision.gameObject.tag == "golden")
+        {
+            Destroy(collision.gameObject);
+            StartInvincible();
+        }
+
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,18 +2,26 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    [Header("Settings")]
+    public float minSpawnDelay;
+    public float maxSpawnDelay;
+
+
 
     [Header("References")]
     public GameObject[] gameObjects;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Invoke("Spawn", Random.Range(minSpawnDelay, maxSpawnDelay));        // 최소 delay와 최대 delay값 사이에서 랜덤
     }
 
     void Spawn() {
+        // Spwner에서 랜덤 오브젝트 생성 
         GameObject randomObject = gameObjects[Random.Range(0, gameObjects.Length)];
 
-        Instantiate(randomObject, transform.position, Quaternion.identity);
+        Instantiate(randomObject, transform.position, Quaternion.identity);     // 마지막값: 오브젝트를 생성할때 별도의 회전값을 주지 않음 
+        Invoke("Spawn", Random.Range(minSpawnDelay, maxSpawnDelay));        // 2초후에 Spawn 호출 
+
     }
 }
